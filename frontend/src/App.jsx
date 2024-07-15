828650affd6123f0fa58ca0ce63f1da700fcdb51
unknown <kiticgoran90@gmail.com>
2024-07-15 14:33:46 +0200

    Initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>Hotel Tajan management system</h1>
@@ -13,4 +7,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
