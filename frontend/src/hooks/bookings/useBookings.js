7108a348109639e2fd255e051733cc91f19cbbad
unknown <kiticgoran90@gmail.com>
2024-07-24 22:40:27 +0200

    Improved pagination - prefetching data

--- frontend/src/hooks/bookings/useBookings.js
+++ frontend/src/hooks/bookings/useBookings.js
@@ -1,13 +1,14 @@
 // hooks
 import { useSearchParams } from "react-router-dom";
 // react-query
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 // services
 import { getBookings } from "../../services/apiBookings";
 // libs
 import { PAGE_SIZE } from "../../utils/constants";
 
 export const useBookings = () => {
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
     // Filter
     const filterValue = searchParams.get("status");
@@ -19,10 +20,26 @@ export const useBookings = () => {
     // Limit
     const limit = PAGE_SIZE;
 
+    // Query data
     const { isLoading, data, error } = useQuery({
         queryKey: ["bookings", filter, sortBy, page],
         queryFn: () => getBookings(filter, sortBy, page, limit)
     });
+
+    // Pre-fetching
+    const pageCount = Math.ceil(data?.totalDocs / PAGE_SIZE);
+    if (page < pageCount) {
+        queryClient.prefetchQuery({
+            queryKey: ["bookings", filter, sortBy, page + 1],
+            queryFn: () => getBookings(filter, sortBy, page + 1, limit)
+        });
+    }
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ["bookings", filter, sortBy, page - 1],
+            queryFn: () => getBookings(filter, sortBy, page - 1, limit)
+        });
+    }
 
     return { isLoading, data, error };
 }
